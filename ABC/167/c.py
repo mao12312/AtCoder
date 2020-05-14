4a338d0c7b00d108f267d16f245e07a8ecb14fb6
mao12312 <42489486+mao12312@users.noreply.github.com>
2020-05-14 23:22:58 +0900

    Tweak

--- ABC/167/c.py
+++ ABC/167/c.py
@@ -1,54 +1,4 @@
 # bit全探索
-# # answer
-# import sys
-# import numpy as np
-# import itertools
-
-# sys.setrecursionlimit(10 ** 9)
-# input = sys.stdin.readline
-
-# """ Input
-# S = input().rstrip()
-# N = int(input())
-# A = np.array(input().split(), dtype=np.float64)
-# D = [int(input()) for _ in range(N)]
-# AB = [[int(x) for x in input().split()] for _ in range(N)]
-# PX = [[int(x) for x in input().split()] for _ in range(Q)]
-# """
-
-# N, M, X = map(int, input().split())
-# algorithm = [0] * M
-
-# prices = []
-# rikai = []
-# for _ in range(N):
-#     C, *A = list(map(int, input().split()))
-#     prices.append(C)
-#     rikai.append(A)
-
-# idx = range(N)
-
-# prices_min = 10 ** 12
-# flag = False
-
-# for i, _ in enumerate(idx, 1):
-#     for candidate in itertools.combinations(idx, r=i):
-
-#         total = 0
-#         for c in candidate:
-#             c_arr = np.array(rikai[c])
-#             total += c_arr
-
-#         if np.all(total >= X):
-#             flag = True
-#             price = 0
-#             for c in candidate:
-#                 price += prices[c]
-
-#             prices_min = min(price, prices_min)
-
-# print(prices_min if flag else '-1')
-
 import itertools
 import numpy as np
 
@@ -57,5 +7,22 @@ CAs = [list(map(int, input().split())) for i in range(N)]
 
 Cs = [ca[0] for ca in CAs]
 As = [ca[1:] for ca in CAs]
+
+ans = -1
+for i in range(1 << N):
+    t = [0]*M
+    c = 0
+    for j in range(N):
+        if (i >> j)&1 == 0:
+            continue
+        c +=Cs[j]
+        for k in range(M):
+            t[k] += As[j][k]
+    if all(x >=X for x in t):
+        if ans == -1:
+            ans = c
+        else:
+            ans = min(ans,c)
+print(ans)
 
 
