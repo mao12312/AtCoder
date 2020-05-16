cfafd7add0e13bc262279fc734aa173d20dec0ab
mao12312 <42489486+mao12312@users.noreply.github.com>
2020-05-16 23:09:50 +0900

    Tweak

--- Study/ITP1_7_A.py
+++ Study/ITP1_7_A.py
@@ -1,5 +1,22 @@
+grade = []
 while True:
-    m,f,r = map(int,input().split())
-    if m and f and r == -1:
+    m, f, r = map(int, input().split())
+    if m == -1 and f == -1 and r == -1:
         break
-
+    elif m == -1 or f == -1:
+        grade.append("F")
+    elif m + f >= 80:
+        grade.append("A")
+    elif 65 <= m + f < 80:
+        grade.append("B")
+    elif 50 <= m + f < 65:
+        grade.append("C")
+    elif 30 <= m + f < 50:
+        if r >= 50:
+            grade.append("C")
+        else:
+            grade.append("D")
+    elif m + f < 30:
+        grade.append("F")
+for i in grade:
+    print(i)
