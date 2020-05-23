5d7c9ca3bde5c3842e393952b773efb2fd1c0b60
mao12312 <42489486+mao12312@users.noreply.github.com>
2020-05-23 23:27:20 +0900

    Tweak

--- Study/ITP1_7_D.py
+++ Study/ITP1_7_D.py
@@ -1,6 +1,8 @@
 n,m,l = map(int,input().split())
-A = [[0]*m for i in range(n)]
-B = [[0]*l for i in range(m)]
+A = [list(map(int,input().split())) for i in range(n)]
+B = [list(map(int, input().split())) for i in range(m)]
 
 print(A)
 print(B)
+
+        
