804c8296ffdf2c13f7a6d25c9120fafb550e816c
mao12312 <42489486+mao12312@users.noreply.github.com>
2020-05-25 23:53:38 +0900

    add problem

--- Study/ITP1_7_D.py
+++ Study/ITP1_7_D.py
@@ -1,8 +1,12 @@
 n,m,l = map(int,input().split())
 A = [list(map(int,input().split())) for i in range(n)]
 B = [list(map(int, input().split())) for i in range(m)]
+ANS = [[0]*l for i in range(n)]
 
-print(A)
-print(B)
 
-        
+for i in range(n):
+    for j in range(l):
+        for k in range(m):
+            ANS[i][j] += A[i][k]*B[k][j]
+for i in ANS:
+    print(i)
