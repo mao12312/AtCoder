a0b380db4725857b300d0fae2831c33f874ba41c
mao12312 <42489486+mao12312@users.noreply.github.com>
2020-06-25 22:53:17 +0900

    Tweak

--- Study/ITP1_9_D_def.py
+++ Study/ITP1_9_D_def.py
@@ -1,22 +1,25 @@
-def Reverce(S, a, b):
-    S[a:b+1] = reversed(S[a:b+1])
-    return S[a:b+1]
+def s_print(s, line):
+    a, b = map(int, line.split()[1:])
+    print(s[a:b + 1])
+    return s
 
 
-def Replace(S, a, b, c):
-    S[a:b+1] = c[0]
-    return S[a:b+1]
+def s_reverse(s, line):
+    a, b = map(int, line.split()[1:])
+    return s[:a] + s[a:b + 1][::-1] + s[b + 1:]
 
 
-S = list(input())
-q = int(input())
-for i in range(q):
-    cmd, a, b, *c = input().split()
-    a = int(a)
-    b = int(b)
-    if cmd == "replace":
-        S = Replace(S, a, b, c)
-    elif cmd == "reverse":
-        S = Reverce(S, a, b)
-    else:
-        print(*S[a:b+1], sep='')
+def s_replace(s, line):
+    ls = line.split()
+    a, b = map(int, ls[1:3])
+    rep = ls[-1]
+    return s[:a] + rep + s[b + 1:]
+
+
+func_dict = {'pri': s_print, 'rev': s_reverse, 'rep': s_replace}
+
+s, q = input(), int(input())
+while q:
+    line = input()
+    s = func_dict[line[:3]](s, line)
+    q -= 1
