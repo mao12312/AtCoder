1d3cdf8064dde694bf215e0936207cab58094b66
mao12312 <42489486+mao12312@users.noreply.github.com>
2020-06-29 23:57:12 +0900

    add problem

--- Study/ITP1_10_B.py
+++ Study/ITP1_10_B.py
@@ -1,1 +1,9 @@
+import math
 a, b, C = map(int, input().split())
+
+S = a * b / 2 * math.sin(math.radians(C))
+c = math.sqrt(a ** 2 + b ** 2)
+L = a + b + c
+h = a*math.tan(math.radians(C))
+
+print(h)
