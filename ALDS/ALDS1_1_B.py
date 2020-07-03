af9fc7fcb286c07e3636c5fb5da7be9570a014af
mao12312 <42489486+mao12312@users.noreply.github.com>
2020-07-03 17:45:15 +0900

    add problem

--- ALDS/ALDS1_1_B.py
+++ ALDS/ALDS1_1_B.py
@@ -1,5 +1,10 @@
 def gcd(x,y):
-    if x >= y:
-        return 
+    if x < y:
+        x,y = y,x
+    if y == 0:
+        return x
+    return gcd(y,x%y)
 
 x,y = map(int,input().split())
+ans = gcd(x,y)
+print(ans)
