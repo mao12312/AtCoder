20f070b35a2043bbc208b18ed0c299cfe1385646
mao12312 <42489486+mao12312@users.noreply.github.com>
2020-07-08 23:42:51 +0900

    add problem

--- ALDS/ALDS1_2_C.py
+++ ALDS/ALDS1_2_C.py
@@ -1,12 +1,31 @@
+import copy
+
+
 def BubbleSort(C, n):
-    for i in range(n):
-        for j in range(n-1, i+1, -1):
-            if int(C[j][1]) < int(C[j-1][1]):
-                C[j], C[j-1] = C[j-1], C[j]
+    flag = 1
+    while flag:
+        flag = 0
+        for i in range(n-1, 0, -1):
+            if int(C[i][1]) < int(C[i - 1][1]):
+                C[i], C[i-1] = C[i-1], C[i]
+                flag = 1
+    return C
+
+
+def SelectionSort(C, n):
+    for i in range(n-1):
+        minj = i
+        for j in range(i, n):
+            if int(C[j][1]) < int(C[minj][1]):
+                minj = j
+        if minj != i:
+            C[i], C[minj] = C[minj], C[i]
     return C
 
 
 n = int(input())
 C = list(input().split())
+C2 = C.copy()
 
 print(BubbleSort(C, n))
+print(SelectionSort(C2, n))
