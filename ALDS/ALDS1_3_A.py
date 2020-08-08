f4f864c61ebc4f5f06a16633e78229cb7f264deb
mao12312 <42489486+mao12312@users.noreply.github.com>
2020-08-08 23:44:27 +0900

    add problem

---
+++ ALDS/ALDS1_3_A.py
@@ -0,0 +1,14 @@
+arr = list(input().split())
+stack = []
+
+for i in arr:
+    if i == "+":
+        stack.append(stack.pop()+stack.pop())
+    elif i == "-":
+        stack.append(-(stack.pop()-stack.pop()))
+    elif i =="*":
+        stack.append(stack.pop()*stack.pop())
+    else:
+        stack.append(int(i))
+print(stack[-1])
+
