29a51e83b5cfd8697fbb28275dfb66c1ccc19ec0
mao12312 <42489486+mao12312@users.noreply.github.com>
2020-09-01 22:03:52 +0900

    add problem

--- ALDS/ALDS1_4_B.py
+++ ALDS/ALDS1_4_B.py
@@ -1,25 +1,54 @@
-n = int(input())
-S = list(map(int, input().split()))
-q = int(input())
-T = list(map(int, input().split()))
+# n = int(input())
+# S = list(map(int, input().split()))
+# q = int(input())
+# T = list(map(int, input().split()))
 
 
-def binarySearch(data, value):
-    left = 0
-    right = len(data) - 1
-    while left <= right:
-        mid = (left + right) // 2
-        if data[mid] == value:
-            return mid
-        elif data[mid] < value:
-            left = mid + 1
+# def binarySearch(data, value):
+#     left = 0
+#     right = len(data) - 1
+#     while left <= right:
+#         mid = (left + right) // 2
+#         if data[mid] == value:
+#             return mid
+#         elif data[mid] < value:
+#             left = mid + 1
+#         else:
+#             right = mid - 1
+#     return -1
+
+
+# ans = []
+# for j in range(q):
+#     if binarySearch(S, T[j]) > -1:
+#         ans.append(binarySearch(S, T[j]))
+# print(len(ans))
+import sys
+
+
+def s():
+    n, k = map(int, input().split())
+    w = list(map(int, sys.stdin.readlines()))
+
+    def f():
+        c = t = 0
+        for j in w:
+            t += j
+            if t > m:
+                t = j
+                c += 1
+                if c >= k:
+                    return 0
+        return 1
+    l, r = max(w), sum(w)
+    while l < r:
+        m = (l+r)//2
+        if f():
+            r = m
         else:
-            right = mid - 1
-    return -1
+            l = m+1
+    print(r)
 
 
-ans = []
-for j in range(q):
-    if binarySearch(S, T[j]) > -1:
-        ans.append(binarySearch(S, T[j]))
-print(len(ans))
+if'__main__' == __name__:
+    s()
--- ALDS/ALDS1_4_D.py
+++ ALDS/ALDS1_4_D.py
@@ -1,4 +1,30 @@
+import sys
 n, k = map(int, input().split())
-W = [int(input()) for _ in range(n)]
+w = [int(sys.stdin.readline()) for _ in range(n)]
+l, r = max(w), sum(w)
 
 
+def ok():
+    c = 0
+    t = 0
+    for j in w:
+        t += j
+        if t > m:
+            t = j
+            c += 1
+            if c >= k:
+                return 0
+    return 1
+
+
+while l < r:
+    m = (l+r)//2
+    if ok():
+        r = m
+    else:
+        l = m+1
+print(r)
+
+
+# 参考
+# http: // www.graco.c.u-tokyo.ac.jp/icpc-challenge/wp-content/uploads/2014/12/2014.pdf
