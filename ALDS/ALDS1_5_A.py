81f040c2900cefd272e78259757ec9f3102130c9
mao12312 <42489486+mao12312@users.noreply.github.com>
2020-09-04 00:30:56 +0900

    Tweak

--- ALDS/ALDS1_5_A.py
+++ ALDS/ALDS1_5_A.py
@@ -9,3 +9,15 @@ m = list(map(int, sys.stdin.readline().split()))
 # print(a)
 # print(q)
 # print(m)
+
+s = set()
+for i in a:
+    lst = list(s)
+    for j in lst:
+        s.add(i+j)
+    s.add(i)
+for i in m:
+    if i in s:
+        print("yes")
+    else:
+        print("no")
