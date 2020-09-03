55e8ebc485796e5c5acee240cd61693db28bfdbb
mao12312 <42489486+mao12312@users.noreply.github.com>
2020-09-03 23:53:05 +0900

    add problem

---
+++ ALDS/ALDS1_5_A.py
@@ -0,0 +1,11 @@
+import sys
+
+n = int(sys.stdin.readline())
+a = list(map(int, sys.stdin.readline().split()))
+q = int(sys.stdin.readline())
+m = list(map(int, sys.stdin.readline().split()))
+
+# print(n)
+# print(a)
+# print(q)
+# print(m)
